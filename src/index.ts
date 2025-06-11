7d534ecaaa3beee1b60b1ce96a7016fb58be889d
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-06-11 16:42:53 +0200

    feat(main): add cors, open overview route and `auth` routes

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,30 @@
 import Elysia from "elysia";
+import cors from "@elysiajs/cors";
+import auth from "~/routers/auth";
+import config from "~/config";
+import pkg from "../package.json";
+import { memoryUsage } from "bun:jsc";
 
-new Elysia().get("/", "Hello, Soundable!").listen(10500);
+new Elysia()
+  .use(
+    cors({
+      origin: true, // Allow every origins so everyone can implement their own client for Soundable.
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      credentials: true,
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  )
+
+  .get("/", () => ({
+    version: pkg.version,
+    bunVersion: Bun.version,
+    bunRevision: Bun.revision,
+    memory: memoryUsage(),
+  }))
+
+  // Diverse routers for the app.
+  .use(auth)
+
+  .listen({ hostname: config.hostname, port: config.port }, (server) => {
+    console.log(`[HTTP]: listening on ${server.url} `);
+  });
