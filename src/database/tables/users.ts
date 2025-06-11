13ce827945c7f51123bb5bde4833278bafab7056
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-06-11 16:41:18 +0200

    feat(drizzle): add `users` table base schema

---
+++ src/database/tables/users.ts
@@ -0,0 +1,55 @@
+import { sql } from "drizzle-orm";
+import {
+  text,
+  sqliteTable as table,
+  uniqueIndex,
+  integer,
+} from "drizzle-orm/sqlite-core";
+
+export const users = table(
+  "users",
+  {
+    id: text()
+      .primaryKey()
+      .$defaultFn(() => Bun.randomUUIDv7("base64url")),
+
+    /**
+     * Username of the user, will be used to authenticate.
+     *
+     * - Should be more than 3 characters long but less than 30 characters long.
+     * - Should only contain alphanumeric characters, underscores, and dots.
+     */
+    username: text({ length: 30 }).notNull(),
+
+    /**
+     * Users can customize their display name so it brings more style
+     * instead of a simple username with no meaning.
+     *
+     * - Should be less than 200 characters.
+     */
+    displayName: text({ length: 200 }),
+
+    /**
+     * Hashed password, hashed using Bun's
+     * [argon2id](https://en.wikipedia.org/wiki/Argon2) implementation.
+     */
+    password: text().notNull(),
+
+    /**
+     * Timestamp when the user was created.
+     */
+    createdAt: integer({ mode: "timestamp" })
+      .notNull()
+      .default(sql`(unixepoch())`),
+
+    /**
+     * Timestamp of the last time the user got updated,
+     * `username`, `displayName` or `password` change.
+     */
+    updatedAt: integer({ mode: "timestamp" })
+      .notNull()
+      .default(sql`(unixepoch())`)
+      .$onUpdate(() => sql`(unixepoch())`),
+  },
+  (table) => [uniqueIndex("username_idx").on(table.username)]
+);
