7928b9c03289448b35e7c0a172642263c99d5ebf
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-06-20 11:44:12 +0200

    feat: add swagger with custom error mesage

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,22 @@ import auth from "~/routers/auth";
 import config from "~/config";
 import pkg from "../package.json";
 import { memoryUsage } from "bun:jsc";
+import { swagger } from "@elysiajs/swagger";
 
 new Elysia()
+  .use(
+    swagger({
+      documentation: {
+        info: {
+          title: "Soundable API",
+          version: pkg.version,
+          description:
+            "A fully self-hosted music library for you and your friends. What you see is the core foundation of Soundable, this is where it all starts.",
+        },
+      },
+    })
+  )
+
   .use(
     cors({
       origin: true, // Allow every origins so everyone can implement their own client for Soundable.
@@ -15,16 +29,24 @@ new Elysia()
     })
   )
 
-  .get("/", () => ({
+  .onError(({ code }) => {
+    return {
+      ok: false,
+      code,
+    };
+  })
+
+  .get("/", ({ server }) => ({
+    ok: true,
     version: pkg.version,
-    bunVersion: Bun.version,
-    bunRevision: Bun.revision,
-    memory: memoryUsage(),
+    bun_version: Bun.version_with_sha,
+    memory_usage_mb: memoryUsage().current / 8_000_000,
+    documentation: new URL("/swagger", server!.url),
   }))
 
   // Diverse routers for the app.
   .use(auth)
 
   .listen({ hostname: config.hostname, port: config.port }, (server) => {
-    console.log(`[HTTP]: listening on ${server.url} `);
+    console.log(`[http]: listening on ${server.url} `);
   });
