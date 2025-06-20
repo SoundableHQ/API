fbd28a88f44db1a1f36dfe898e9486568d234cc0
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-06-20 11:45:52 +0200

    feat(router): add `sign-up` and `sign-in` routes with retrieval route for auth

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,3 +1,104 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
+import auth from "~/macros/auth";
+import config from "~/config";
+import database from "~/database";
+import * as tables from "~/database/schema";
+import { eq } from "drizzle-orm";
 
-export default new Elysia({ prefix: "auth" });
+export default new Elysia({ prefix: "auth" })
+  .use(auth)
+  .get("/", ({ user: { password, ...user } }) => ({ ok: true, user }), {
+    auth: true,
+  })
+  .post(
+    "/sign-up",
+    async ({ body, status, generateToken }) => {
+      if (!config.allowSignUp)
+        return status(400, {
+          ok: false,
+          code: "SIGN_UP_DISALLOWED",
+        });
+
+      const username = body.username;
+
+      {
+        const user = database
+          .select()
+          .from(tables.users)
+          .where(eq(tables.users.username, username))
+          .get();
+
+        if (user)
+          return status(400, {
+            ok: false,
+            code: "USERNAME_ALREADY_TAKEN",
+          });
+      }
+
+      const password = await Bun.password.hash(body.password);
+
+      try {
+        const user = database
+          .insert(tables.users)
+          .values({
+            username,
+            password,
+          })
+          .returning()
+          .get();
+
+        const token = await generateToken(user.id);
+
+        return status(200, {
+          ok: true,
+          token,
+        });
+      } catch {
+        throw status(500, {
+          ok: false,
+          code: "ERROR_DATABASE",
+        });
+      }
+    },
+    {
+      body: t.Object({
+        username: t.String({ minLength: 3, maxLength: 30 }),
+        password: t.String({ minLength: 8, maxLength: 50 }),
+      }),
+    }
+  )
+  .post(
+    "/sign-in",
+    async ({ body, status, generateToken }) => {
+      const user = database
+        .select()
+        .from(tables.users)
+        .where(eq(tables.users.username, body.username))
+        .get();
+
+      if (!user)
+        return status(400, {
+          ok: false,
+          code: "NO_ACCOUNT",
+        });
+
+      if (!(await Bun.password.verify(body.password, user.password)))
+        return status(403, {
+          ok: false,
+          code: "PASSWORD_INVALID",
+        });
+
+      const token = await generateToken(user.id);
+
+      return status(200, {
+        ok: true,
+        token,
+      });
+    },
+    {
+      body: t.Object({
+        username: t.String({ minLength: 3, maxLength: 30 }),
+        password: t.String({ minLength: 8, maxLength: 50 }),
+      }),
+    }
+  );
